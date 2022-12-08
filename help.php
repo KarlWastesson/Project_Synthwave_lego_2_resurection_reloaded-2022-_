158749b37a35749503eb0d9fc3b7a55a4c020648
dasooni <dr.amit@live.se>
2022-12-08 15:50:07 +0100

    update help

--- help.php
+++ help.php
@@ -10,7 +10,8 @@
        </div>
         <div class="result">
 	  	  <p class = "title"> <strong> About this page </strong></p>	
-        <div class="content"> This is a lego database. </div>			
+        <div class="content"> This is a website for our project in the course "Electronic publishing" at Linköping university in Norrköping, Sweden. The goal was to create a website
+          that will host a database for lego parts. <br> <br> Thank you. <br> Dasmit, Karl, Klas, Ramez. </div>			
      </div>    
      
   </body>
