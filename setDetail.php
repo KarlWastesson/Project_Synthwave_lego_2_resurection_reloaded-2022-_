be30ea6e0d932a2271f90b6deddec390d0681c67
Ramez96 <45939926+Ramez96@users.noreply.github.com>
2022-12-09 21:58:31 +0100

    Add files via upload
    Uppdaterade bakgrunden CSS
    
    Ändrade så att sets visas i rutor, ändras med skärmstorleken,

--- setDetail.php
+++ setDetail.php
@@ -3,36 +3,40 @@
 
 <body>
 	<div class="flexContainer">
-    	 <div class="header">
-   			 <?php include 'header.html'; ?>	
-   		 </div>
-    <div class="bar">
-    <?php include 'search.php'; ?>	
-    </div>
-	<div class="photoCardDetail">
-		<?php 
+		<div class="header">
+			<?php include 'header.html'; ?>
+		</div>
+		<div class="bar">
+			<?php include 'search.php'; ?>
+		</div>
+		<div class="photoCardDetail">
+			<?php
 			$SetID =  $_GET['setID'];
 			$SetName =  $_GET['setName'];
 			$SetPicture =  $_GET['setPicture'];
 
-			if($SetPicture){
-			echo("<img src=\"$SetPicture\" alt=\"NO image\"/>");
-			}
-			else{
-			echo('<th><img src="noimage_small.png" alt="NO image"/></th>');
+			if ($SetPicture) {
+				echo ("<img src=\"$SetPicture\" alt=\"NO image\"/>");
+			} else {
+				echo ('<th><img src="noimage_small.png" alt="NO image"/></th>');
 			}
 
-       	    echo("<h1>Set of $SetID $SetName</h1>"); 
-		 ?>	
-	</div>
-		<div class="result"> 	
+			echo ("<h1>Set of $SetID $SetName</h1>");
+			?>
+		</div>
+		<div class="result">
 			<table class="tableDetail" id="partTable">
 				<tbody>
-					<tr><th>Quantity</th><th>Picture</th><th>Color</th><th>Part name</th></tr>
-			 		<?php printTable();?>
+					<tr>
+						<th>Quantity</th>
+						<th>Picture</th>
+						<th>Color</th>
+						<th>Part name</th>
+					</tr>
+					<?php printTable(); ?>
 				</tbody>
 			</table>
-		</div>	
+		</div>
 	</div>
 </body>
 
@@ -41,10 +45,11 @@
 
 <?php
 
-function printTable() {
+function printTable()
+{
 	$SetID =  $_GET['setID'];
-	$connection=mysqli_connect("mysql.itn.liu.se","lego","","lego");	//Lego is the db
-		
+	$connection = mysqli_connect("mysql.itn.liu.se", "lego", "", "lego");	//Lego is the db
+
 	$sql = "SELECT inventory.Quantity, inventory.ItemTypeID, inventory.ItemID, 
 	inventory.ColorID, colors.Colorname, parts.Partname, images.has_gif, 
 	images.has_jpg, images.has_largegif, images.has_largejpg
@@ -61,43 +66,42 @@ function printTable() {
 		AND inventory.ItemTypeID = 'P'
 	
 	ORDER BY inventory.ItemID";
-	
-	
 
-    $result = mysqli_query($connection, $sql);
+
+
+	$result = mysqli_query($connection, $sql);
 
 
 
 	while ($row = mysqli_fetch_array($result)) {
 
-			$quantity   = $row['Quantity']; 
-			$itemTypeID = $row['ItemTypeID']; //little middle t
-			$itemID     = $row['ItemID'];
-			$colorID    = $row['ColorID'];
-			$has_gif    = $row['has_gif'];
-			$has_jpg    = $row['has_jpg'];
-			$colorname  = $row['Colorname'];
-			$partname   = $row['Partname'];
-			$url 		= "";
-			$prefix		= "http://weber.itn.liu.se/~stegu/img.bricklink.com/";
-	
+		$quantity   = $row['Quantity'];
+		$itemTypeID = $row['ItemTypeID']; //little middle t
+		$itemID     = $row['ItemID'];
+		$colorID    = $row['ColorID'];
+		$has_gif    = $row['has_gif'];
+		$has_jpg    = $row['has_jpg'];
+		$colorname  = $row['Colorname'];
+		$partname   = $row['Partname'];
+		$url 		= "";
+		$prefix		= "http://weber.itn.liu.se/~stegu/img.bricklink.com/";
+
 		if ($has_gif == 1) {
 			$url = $itemTypeID . "/" . $colorID . "/" . $itemID . ".gif";
 		} else if ($has_jpg == 1) {
 			$url = $itemTypeID . "/" . $colorID . "/" . $itemID . ".jpg";
 		}
 		//rows ctrl D
-		print ("<tr>\n");
+		print("<tr>\n");
 
-		print("<th id=\"row1\">".$quantity."</th>\n");
-		print("<th><image src=".$prefix.$url."></image></th>\n");
-		print("<th>".$colorname."</th>\n");
-		print("<th>".$partname."</th>\n");
-		print ("</tr>");
-			
-	}  
-	    
+		print("<th id=\"row1\">" . $quantity . "</th>\n");
+		print("<th><image src=" . $prefix . $url . "></image></th>\n");
+		print("<th>" . $colorname . "</th>\n");
+		print("<th>" . $partname . "</th>\n");
+		print("</tr>");
+	}
 
-		mysqli_close($connection);
+
+	mysqli_close($connection);
 }
 ?>
--- setSearch.php
+++ setSearch.php
@@ -2,45 +2,42 @@
 <html lang="en">
 
 <body>
-<div class="flexContainer">
-    <div class="header">
-    <?php include 'header.html'; ?>	
+    <div class="flexContainer">
+        <div class="header">
+            <?php include 'header.html'; ?>
+        </div>
+        <div class="bar">
+            <form method="GET" action="setSearch.php">
+                <?php include 'search.php'; ?>
+            </form>
+            <div id="suggestion">
+            </div>
+        </div>
+        <?php
+        $SetID =  $_GET['set'];
+        echo "<h2 style='color:white'>Result of $SetID</h2>";
+        ?>
+        <div class="grid-container">
+            <?php printTable(); ?>
+        </div>
     </div>
-    <div class="bar">
-    <form method="GET" action="setSearch.php"> 		
-    <?php include 'search.php'; ?>	
-    </form>
-    </div> 
-		<div class="result">
-			<?php  
-	        	$SetID =  $_GET['set'];		
-	            	echo "<h2>Result of $SetID</h2>"; ?>		
-			<table class="tableSet ">
-			<tbody>
-			<tr><th>SetID</th><th>SetName</th><th>Image</th>
-			 <?php printTable(); ?>
-			</tbody>
-		</table>	
-	</div>	
-</div>
 </body>
-</html>
 
+</html>
 
 <?php
 
-function printTable() {
+function printTable()
+{
     //remove whitespace!
     $search = trim($_GET['set']);
-    $term = "'%".$search."%'";
+    $term = "'%" . $search . "%'";
     //Some sort of limiter needs to be smarter
-    if(empty($search)) {
+    if (empty($search)) {
         $limit = 5;
-     }
-     else
-     {
-        $limit = 20;
-     }
+    } else {
+        $limit = 30;
+    }
     $con = mysqli_connect("mysql.itn.liu.se", "lego", "", "lego");
 
     $query = buildSqlQuery($term, $limit);
@@ -50,45 +47,43 @@ function printTable() {
         $setID = $row['SetID'];
         $setName = $row['Setname'];
         $image = fetchImage($con, $setID);
-
-        print ("<tr>\n");
-        print("<th><a href='setDetail.php?setID=$setID&setName=$setName&setPicture=$image'>$setID</a></th>");
-        print("<th>$setName</th>");
+        //första print skapar hela "divven" sen pushas allt därefter. </a> läggs till automatiskt i slutet ¯\_(ツ)_/¯
+        print("<a href='setDetail.php?setID=$setID&setName=$setName&setPicture=$image'>");
+        print("<p>$setName</p>");
+        print("<p>$setID</p>");
 
         if ($image) {
-            print("<th><img id=\"setPicture\" src=\"$image\" alt=\"NO image\"/></th>");
+            print("<img id=\"setPicture\" src=\"$image\" alt=\"NO image\"/>");
         } else {
-            print('<th><img id=\"setPicture\" src="noimage_small.png" alt="NO image"/></th>');
+            print('<img id=\"setPicture\" src="noimage_small.png" alt="NO image"/>');
         }
     }
-	mysqli_close($con);
+    mysqli_close($con);
 }
 
-function buildSqlQuery($term, $limit) {
+function buildSqlQuery($term, $limit)
+{
     return "SELECT Setname, SetID FROM sets WHERE Setname LIKE $term OR SetID LIKE $term LIMIT $limit";
 }
 
-function fetchImage($con, $setID) {
+function fetchImage($con, $setID)
+{
     $imagesearch = mysqli_query($con, "SELECT images.has_gif, images.has_jpg, images.has_largegif, images.has_largejpg FROM images WHERE images.ItemID=$setID");
     $imageinfo = mysqli_fetch_array($imagesearch);
     $prefix = "http://www.itn.liu.se/~stegu76/img.bricklink.com/";
 
-    if ($ $imageinfo['has_largegif'] || $imageinfo['has_largejpg']){
-    
-        if($imageinfo['has_largegif']){
-            return "$prefix/SL./$setID.gif"; 
-             
-        }
-        else{
+    if ($$imageinfo['has_largegif'] || $imageinfo['has_largejpg']) {
+
+        if ($imageinfo['has_largegif']) {
+            return "$prefix/SL./$setID.gif";
+        } else {
             return "$prefix/SL/$setID.jpg";
         }
-    }
-    else if( $imageinfo['has_gif'] ||  $imageinfo['has_jpg']){
-        
-        if($imageinfo['has_gif']){
-           return "$prefix/S/$setID.gif";
-        }
-        else{
+    } else if ($imageinfo['has_gif'] ||  $imageinfo['has_jpg']) {
+
+        if ($imageinfo['has_gif']) {
+            return "$prefix/S/$setID.gif";
+        } else {
             return "$prefix/S/$setID.jpg";
         }
     }
